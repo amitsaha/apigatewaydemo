6567060ded83db041b50dd0cfc8b0ed30b76c073
Amit Saha <amitsaha.in@gmail.com>
2016-11-17 17:00:34 +1100

    Python web app: use a service id for registering and deregister on exit

--- webapp-1/app.py
+++ webapp-1/app.py
@@ -1,9 +1,21 @@
 from flask import Flask, request, jsonify
 import consulate
+import os
+import signal
+import sys
+
+port = int(os.environ.get('PORT', 5000))
+service_id = 'projects_%s' % port
 
 consul = consulate.Consul()
 # Add "projects" service to the local agent
-consul.agent.service.register('projects', port=5000)
+consul.agent.service.register('projects', service_id=service_id, port=port)
+
+
+def signal_handler(signal, frame):
+    consul.agent.service.deregister(service_id)
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 
 app = Flask(__name__)
 
@@ -16,4 +28,4 @@ def create_project():
     return jsonify(id=id, url="Project-%s" % id)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=port)
