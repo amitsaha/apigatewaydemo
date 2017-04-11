76d5162fe8446c9b391508a426f5aed7eb18cf97
Amit Saha <amitsaha.in@gmail.com>
2017-04-11 16:16:45 +1000

    webapp1: kubernetes files

--- webapp-1/app.py
+++ webapp-1/app.py
@@ -1,29 +1,16 @@
 from flask import Flask, request, jsonify
-import consulate
 import os
-import signal
-import sys
-
-port = int(os.environ.get('PORT', 5000))
-service_id = 'projects_%s' % port
-
-consul = consulate.Consul()
-# Add "projects" service to the local agent
-consul.agent.service.register('projects', service_id=service_id, port=port)
-
-
-def signal_handler(signal, frame):
-    consul.agent.service.deregister(service_id)
-    sys.exit(0)
-signal.signal(signal.SIGINT, signal_handler)
 
 app = Flask(__name__)
-
 
 @app.route('/create', methods=['POST'])
 def create_project():
     id = 123
     return jsonify(id=id, url="Project-%s" % request.json.get('title'))
 
+@app.route('/_status/healthcheck/')
+def healtchechk():
+    return 'OK'
+
 if __name__ == '__main__':
-    app.run(debug=True, port=port)
+    app.run()
